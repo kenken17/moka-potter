9fc8d8e76497e56c2ddb4a2417b08ee996592b3d
ken <kenken17@gmail.com>
2026-04-27 23:36:10 +0800

    fix: rating auto-save and show 0/10 by default
    - Fix rating save on detail page by creating new cup/brew objects
      instead of mutating nested properties directly
    - Show '0/10' text even when no rating selected to prevent layout shift
    - Apply same fix to form rating display

--- src/views/BrewDetail.vue
+++ src/views/BrewDetail.vue
@@ -54,7 +54,7 @@
                   <svg width="14" height="14" viewBox="0 0 24 24" :fill="n <= cup.rating ? 'currentColor' : 'none'" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                 </button>
               </div>
-              <span v-if="cup.rating > 0" class="cup-rating-text">{{ cup.rating }}/10</span>
+              <span class="cup-rating-text">{{ cup.rating || 0 }}/10</span>
             </div>
           </div>
 
@@ -107,7 +107,9 @@ function goBack() {
 
 async function setRating(cupIndex, rating) {
   if (!brew.value) return
-  brew.value.cups[cupIndex].rating = rating
+  const updatedCups = [...brew.value.cups]
+  updatedCups[cupIndex] = { ...updatedCups[cupIndex], rating }
+  brew.value = { ...brew.value, cups: updatedCups }
   await updateBrew(brew.value)
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -120,7 +120,7 @@
                       <svg width="16" height="16" viewBox="0 0 24 24" :fill="n <= cup.rating ? 'currentColor' : 'none'" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
                     </button>
                   </div>
-                  <span v-if="cup.rating > 0" class="rating-text">{{ cup.rating }}/10</span>
+                  <span class="rating-text">{{ cup.rating || 0 }}/10</span>
                 </div>
               </div>
 
