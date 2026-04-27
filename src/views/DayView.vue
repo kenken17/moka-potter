18a6f75d14c386ff046f7079ad924f8f8da800c2
ken <kenken17@gmail.com>
2026-04-27 23:55:21 +0800

    fix: day view title format to Mon, 27 Apr 2025

--- src/views/DayView.vue
+++ src/views/DayView.vue
@@ -56,7 +56,7 @@ const dayTitle = computed(() => {
   const dateStr = route.params.date
   if (!dateStr) return ''
   try {
-    return format(parseISO(dateStr), 'EEE, MMM d')
+    return format(parseISO(dateStr), 'EEE, d MMM yyyy')
   } catch {
     return dateStr
   }
